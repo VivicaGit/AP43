af0febb66d3445923533db69a70514dfb0eeb317
vivica <vivica@alumnatflorida.es>
2025-10-27 10:03:48 +0100

    Ejercicio 2

---
+++ ej2.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+<?php
+$countryString = "Japan,Mexico,USA,India,Korea,China,Nigeria,Argentina,Egypt,England";
+
+$countryArray = explode(",", $countryString);
+
+$capitals = [];
+
+foreach ($countryArray as $country) {
+    $capitals [] = ['country' => $country];
+}
+
+var_dump($capitals);
+
+?>
+
+</body>
+
+</html>
